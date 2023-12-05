a0180db011e04c63dcd3680b5cfaad785c546160
육종호 <dbrwhdgh1203@naver.com>
2023-12-06 00:38:13 +0900

    implement: skeleton code

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import Header from "./layout/header";
+
+function App() {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
+}
+
+export default App;
---
+++ my-app/src/layout/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+    width: auto;
+    height: 200px;
+    border: 1px solid black;
+`;
+
+const Header = () => {
+    return <Main></Main>;
+};
+
+export default Header;
